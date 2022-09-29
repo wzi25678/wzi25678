c0e28a05ccf69845cd1a7179206ec2dc074a474f
当然是一一呀！ <84449746+wzi25678@users.noreply.github.com>
2022-09-29 19:36:31 +0800

    Create 程序4-21 concentric_circles.py

---
+++ Python/Python基础概论-课本代码/程序4-21 concentric_circles.py
@@ -0,0 +1,33 @@
+# 学习作者： Wzi
+# 开发时间： 2022/7/20 14：05
+# 学好Python，搞定大数据
+# import turtle
+# turtle.showturtle()
+# turtle.done()
+# import turtle
+# for num in range(1,9):
+#     turtle.forward(50)
+#     turtle.left(45)
+#     num+=1
+# turtle.done()
+
+#    程序4-21   concentric_circles.py
+#    Concentric circles
+import turtle
+NUM_CIRCLES = 45
+STARTING_RADIUS = 20
+OFFSET = 10
+ANIMATION_SOEED = 0
+turtle.speed(ANIMATION_SOEED)
+turtle.hideturtle()
+radius = STARTING_RADIUS
+for count in range(NUM_CIRCLES) :
+   turtle.circle(radius)
+   x = turtle.xcor()
+   y = turtle.ycor()-OFFSET    #turtle.circle(radius)是以当前位置为圆的正下角，逆时针绕一个以radius为半径的圆
+   radius = radius +OFFSET
+   turtle.penup()
+   turtle.goto(x,y)
+   turtle.pendown()
+turtle.done()
+
