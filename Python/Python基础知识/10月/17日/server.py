17c9961e9a98dbe2fa116c61f6a485f8a7b19b3c
当然是一一呀！ <84449746+wzi25678@users.noreply.github.com>
2022-10-17 11:23:48 +0800

    Create server.py

---
+++ Python/Python基础知识/10月/17日/server.py
@@ -0,0 +1,34 @@
+# 学习作者： Wzi
+# 开发时间： 2022/7/20 14：05
+# 学好Python，搞定大数据
+# !/usr/bin/python3
+# 文件名：server.py
+
+# 导入 socket、sys 模块
+import socket
+import sys
+
+# 创建 socket 对象
+serversocket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+
+# 获取本地主机名
+host = socket.gethostname()
+
+port = 9999
+
+# 绑定端口号
+serversocket.bind((host, port))
+
+# 设置最大连接数，超过后排队
+serversocket.listen(5)
+
+while True:
+    # 建立客户端连接
+    clientsocket, addr = serversocket.accept()
+
+    print("连接地址: %s" % str(addr))
+
+    msg = '欢迎访问菜鸟教程！' + "\r\n"
+    clientsocket.send(msg.encode('utf-8'))
+    clientsocket.close()
